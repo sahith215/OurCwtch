3f491f78add9942db235e8d3c102e27326c42abe
Sahith <sahith305@gmail.com>
2026-08-21 11:29:37 +0530

    Install @vercel/node, fix api/index.ts, and remove legacy unused app/api files

--- api/index.ts
+++ api/index.ts
@@ -3,9 +3,9 @@ import { handleApiRequest } from '../app/lib/apiRouter'
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   try {
-    const protocol = req.headers['x-forwarded-proto'] || 'https'
+    const protocol = (req.headers['x-forwarded-proto'] as string) || 'https'
     const host = req.headers.host || 'localhost'
-    const url = new URL(req.url || '/', ${protocol}://System.Management.Automation.Internal.Host.InternalHost)
+    const fullUrl = new URL(req.url || '/', `${protocol}://${host}`)
 
     let bodyBuffer: Buffer | undefined = undefined
     if (!['GET', 'HEAD'].includes(req.method || '')) {
@@ -14,7 +14,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
       }
     }
 
-    const webRequest = new Request(url.toString(), {
+    const webRequest = new Request(fullUrl.toString(), {
       method: req.method,
       headers: req.headers as any,
       body: bodyBuffer,
